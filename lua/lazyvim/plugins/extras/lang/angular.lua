b83070bdf6fcdb5fdd3728e6e4af1830c97bbdd0
Mykhailo Sabatura <mishasbtr@gmail.com>
2024-06-06 09:22:49 +0200

    fix(extra-angular): use autocmd

--- lua/lazyvim/plugins/extras/lang/angular.lua
+++ lua/lazyvim/plugins/extras/lang/angular.lua
@@ -20,7 +20,12 @@ return {
           [".*%.container%.html"] = "angular.html",
         },
       })
-      vim.treesitter.language.register("angular", "angular.html")
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "angular.html",
+        callback = function()
+          vim.treesitter.language.register("angular", "angular.html")
+        end,
+      })
     end,
   },
 
